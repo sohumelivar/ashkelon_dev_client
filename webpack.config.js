716e16aecc556076175990060346002611cd5033
Givi Koyava <sohumelivar@gmail.com>
2024-12-23 21:43:34 +0200

    webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
+    clean: true,
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new webpack.ProgressPlugin(),
+  ],
 };
